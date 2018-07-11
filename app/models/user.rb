68ee89a7d6379ddcf1502c2699e222d062312667
mproud99@gmail.com <mproud99@gmail.com>
2018-07-11 15:12:01 -0400

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable 
+  devise :database_authenticatable, #:registerable,
+         :recoverable, :rememberable, :trackable, :validatable, 
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+     unless user
+        user = User.create(first_name: data['first_name'],
+           last_name: data['last_name'],
+           email: data['email'],
+           password: Devise.friendly_token[0,20],
+           image: data['image']
+        )
+    end
+    user
+  end
+end
---
+++ db/migrate/20180711152747_add_omni_auth_to_users.rb
@@ -0,0 +1,7 @@
+class AddOmniAuthToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :image, :text
+  end
+end
