74f7ab55d577bcec4b5528fa52b59f623fcb25f7
melissakerns <mproud99@gmail.com>
2018-08-01 10:24:06 -0400

    add Call model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_11_172239) do
+ActiveRecord::Schema.define(version: 2018_07_30_142556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "calls", force: :cascade do |t|
+    t.integer "callrail_id"
+    t.datetime "start_time"
+    t.string "caller_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
