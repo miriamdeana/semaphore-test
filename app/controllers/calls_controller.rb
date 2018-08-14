0d98e93986c91cbbe286b469b3d58fc10d36e3cf
Vicky <vii.zhu@gmail.com>
2018-08-14 14:03:25 -0400

    save info into database when call starts

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,4 +1,10 @@
 class CallsController < ApplicationController
+  protect_from_forgery with: :null_session
   def create
+    Call.create!(
+      callrail_id: params[:id],
+      start_time: params[:start_time],
+      caller_number: params[:formatted_customer_phone_number]
+      )
   end
 end
