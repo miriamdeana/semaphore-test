2ec9d23390447af87b06c27d26a0c5722e219398
Vicky <vii.zhu@gmail.com>
2018-08-17 16:32:05 -0400

    working api pull for single call

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,10 +1,16 @@
+require 'callrail_api'
+
 class CallsController < ApplicationController
   protect_from_forgery with: :null_session
   def create
-    Call.create!(
-      callrail_id: params[:id],
-      start_time: params[:start_time],
-      caller_number: params[:formatted_customer_phone_number]
-      )
+    call_id = params[:id]
+    # calls_queue << call_id
+    new_call = Call.create!(
+       callrail_id: params[:id],
+       start_time: params[:start_time],
+       caller_number: params[:formatted_customer_phone_number]
+     )
+    api = CallrailApi.new(call_id).get_call
+    Call.find_by(callrail_id: call_id).update(answered: api["answered"])
   end
 end
