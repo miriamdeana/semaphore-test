becd1009a450a501d19c5023ccd02a1afee9f37c
Vicky <vii.zhu@gmail.com>
2018-08-20 11:27:32 -0400

    add autoload for /lib

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -4,7 +4,6 @@ class CallsController < ApplicationController
   protect_from_forgery with: :null_session
   def create
     call_id = params[:id]
-    # calls_queue << call_id
     new_call = Call.create!(
        callrail_id: params[:id],
        start_time: params[:start_time],
--- app/models/callrail_api.rb
+++ app/models/callrail_api.rb
@@ -1,15 +1,15 @@
-require 'httparty'
+# require 'httparty'
 
-class CallrailApi 
-    include HTTParty
+# class CallrailApi 
+#     include HTTParty
 
-    def initialize(callrail_id)
-        @callrail_id = callrail_id
-    end
+#     def initialize(callrail_id)
+#         @callrail_id = callrail_id
+#     end
     
-    def get_call
-        response = HTTParty.get("https://api.callrail.com/v2/a/266101466/calls/#{@callrail_id}.json?fields=tags",
-                               :headers => { "Authorization" => "Token token=#{Rails.application.credentials.callrail_api_key}" })
-        json = JSON.parse(response.body)
-    end
-end
+#     def get_call
+#         response = HTTParty.get("https://api.callrail.com/v2/a/266101466/calls/#{@callrail_id}.json?fields=tags",
+#                                :headers => { "Authorization" => "Token token=#{Rails.application.credentials.callrail_api_key}" })
+#         json = JSON.parse(response.body)
+#     end
+# end
