abd7b5d7e3b10f774ca88c621c071bbb13f6fc84
Vicky <vii.zhu@gmail.com>
2018-08-23 22:10:30 -0400

    working api ping

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,16 +1,17 @@
 require 'callrail_api'
 
 class CallsController < ApplicationController
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
+  
   def create
     call_id = params[:id]
-    # calls_queue << call_id
     new_call = Call.create!(
        callrail_id: params[:id],
        start_time: params[:start_time],
        caller_number: params[:formatted_customer_phone_number]
      )
     api = CallrailApi.new(call_id).get_call
-    Call.find_by(callrail_id: call_id).update(answered: api["answered"])
+    Call.find_by(callrail_id: call_id).update(answered: api["customer_city"])
   end
 end
