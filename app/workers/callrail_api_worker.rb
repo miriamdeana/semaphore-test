2f41a93d68ffd7c34bbd0c21a1e7a92e7ba44e53
Vicky <vii.zhu@gmail.com>
2018-09-04 14:55:22 -0400

    refactor call answered logic for worker

--- app/workers/callrail_api_worker.rb
+++ app/workers/callrail_api_worker.rb
@@ -9,37 +9,19 @@ class CallrailApiWorker
 		tags = api["tags"]
     answered = api["answered"]
 
-    #puts "#{api}"
-
     if tags.blank?
-      # Check if call has been answered. If not, end.
-      case answered
-      when false
-        puts "#{callrail_id} No answer (#{answered}) or client hung up, were there any tags? #{tags}. End."
-      when true
-        puts "#{callrail_id} Answered (#{answered}) but no tags (#{tags}), try again in 5 seconds"
-        CallrailApiWorker.perform_in(5.seconds, callrail_id)
-      else
-        puts "#{callrail_id} No answer (#{answered}) and no tags (#{tags}), try again in 5 seconds"
+      if answered || answered.nil?
+        puts "#{answered}, #{tags}"
         CallrailApiWorker.perform_in(5.seconds, callrail_id)
       end
 
     elsif tags[0]["name"] == "Support" || tags[0]["name"] == "Billing"
-      # Check if call has been answered. If not, end.
-      case answered
-      when false
-        puts "No answer (#{answered}) or client hung up, were there any tags? (#{tags[0]["name"]}) End."
-      when true
-        puts "Answered (#{answered}) and it was tagged appropriately (#{tags[0]["name"]}). Next step."
-        # NEXT STEP: figure out who is the agent.
-      else
-        puts "No answer (#{answered}) but tagged appropriately (#{tags[0]["name"]}), try again in 5 seconds"
+      if answered
+        #NEXT STEP: Figure out who the agent is
+      elsif answered.nil?
+        puts "#{answered}, #{tags}"
         CallrailApiWorker.perform_in(5.seconds, callrail_id)
       end
-
-    else
-      puts "Not tagged Support or Billing (#{tags[0]["name"]}). End."
     end
-
 	end
 end
