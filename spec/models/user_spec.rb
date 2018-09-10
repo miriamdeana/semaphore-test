333b3dbc353da42fbbcc28f7a64401fc481a5830
melissakerns <mproud99@gmail.com>
2018-09-10 11:32:39 -0400

    change instance variable to let and simplify tests when changing single attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,22 @@ require "rails_helper"
 
 RSpec.describe User do
 
-  before(:each) do
-    @user = build(:user)
-  end
+  let(:user) { build(:user) }
 
   describe "validations" do
     it "is valid with valid attributes" do
-      expect(@user).to be_valid
+      expect(user).to be_valid
     end
 
     it "is invalid if email doesn't end in @callrail.com" do
-      @user = User.new(first_name: "Melissa", last_name: "Kerns", email: "melissa@gmail.com", password: "abcd1234")
-      expect(@user).to be_invalid
+      user = build(:user, email: "melissa@gmail.com")
+      expect(user).to be_invalid
     end
 
     it "should have a unique email address" do
-      @user.save
-      @user2 = User.new(first_name: "Melissa", last_name: "Kerns", email: @user.email, password: "1234abcd")
-      expect(@user2).to be_invalid
+      user.save
+      user2 = build(:user, email: user.email)
+      expect(user2).to be_invalid
     end
   end
 end
