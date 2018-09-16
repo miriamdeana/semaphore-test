d0d6b4cd3334b0e6fcd3c42c5ab064fce969bf19
Vicky <vii.zhu@gmail.com>
2018-09-16 15:50:43 -0400

    add json parse to callrail_api and write test for answered is nil

--- lib/callrail_api.rb
+++ lib/callrail_api.rb
@@ -10,5 +10,6 @@ class CallrailApi
     def get_call
         response = HTTParty.get("https://api.callrail.com/v2/a/266101466/calls/#{@callrail_id}.json?fields=tags",
                                :headers => { "Authorization" => "Token token=#{Rails.application.credentials.callrail_api_key}" })
+        json = JSON.parse(response.body)
     end
 end
--- spec/workers/callrail_api_worker_spec.rb
+++ spec/workers/callrail_api_worker_spec.rb
@@ -4,26 +4,37 @@ RSpec.describe CallrailApiWorker do
   before(:each) do
     Sidekiq::Worker.clear_all
     stub_request(:get, "https://api.callrail.com/v2/a/266101466/calls/1234.json?fields=tags").
-                with(:body => callrail_api_response,
-                      :headers => { "Authorization" => "Token token=#{Rails.application.credentials.callrail_api_key}" })
+                with(:headers => { "Authorization" => "Token token=#{Rails.application.credentials.callrail_api_key}" }).
+                to_return(status: 200, body: callrail_api_response.to_json, headers:{})
   end
 
   let(:callrail_id) { "1234" }
   let(:worker) { CallrailApiWorker.new }
   let(:callrail_api_response) {
-    { 
-      "answered": false
+    {
+      "answered"=>nil,
+      "business_phone_number"=>nil,
+      "customer_city"=>"Atlanta",
+      "customer_country"=>"US",
+      "customer_name"=>"User",
+      "customer_phone_number"=>"+15555555555",
+      "customer_state"=>"GA",
+      "direction"=>"inbound",
+      "duration"=>8,
+      "id"=>1234,
+      "recording"=>nil,
+      "recording_duration"=>nil,
+      "recording_player"=>nil,
+      "start_time"=>"2018-09-16T14:59:53.250-04:00",
+      "tracking_phone_number"=>"+15555555555",
+      "voicemail"=>false,
+      "tags"=>[]
     }
   }
 
-  it "should" do
-    allow(worker).to receive(:ping_api).and_return(callrail_api_response)
-    # expect(worker).to receive(:ping_api).and_return(callrail_api_response)
-    # Sidekiq::Testing.inline! do
-      worker.perform("1234")
-      # assert_equal 1, CallrailApiWorker.jobs.size
-      # CallrailApiWorker.drain
-      # assert_equal 0, CallrailApiWorker.jobs.size
-    # end
+  it "should keep pinging the api when answered is nil" do
+    allow(worker).to receive(:loop).and_yield.and_yield
+    expect(worker).to receive(:sleep).with(5).twice
+    worker.perform("1234")
   end
 end
