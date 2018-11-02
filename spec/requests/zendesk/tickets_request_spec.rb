208c4ad5e661bc69ce70224a3201ee2a3e8381ab
melissakerns <mproud99@gmail.com>
2018-11-02 16:28:28 -0400

    WIP ticket request spec

--- spec/requests/zendesk/tickets_request_spec.rb
+++ spec/requests/zendesk/tickets_request_spec.rb
@@ -6,25 +6,19 @@ describe 'Tickets' do
     to_return(status: 200, body: "", headers: {})
   end
 
-  let(:create_ticket) { post zendesk_tickets_create_url,
-    :params => {
-      :subject => 'Phone Call Ticket',
-      :submitter_id => 370404079212,
-      :tags => ['inboundcallticket'],
-      :comment => { :value => 'Description of call.', :public => false },
-      :requester => { :name => "Mr Customer",
-                      :email => "customer@example.com",
-                      :phone => '555-555-5555'
-                    }
-    }
-  }
-
   describe 'Ticket Creation' do
     it 'should initiate a post request to Zendesk' do
-      create_ticket
-      body = '{"ticket":{"subject":"CTI Phone Call Ticket","requester":{"name": "Mr Customer","email": "customer@example.com","phone": "555-555-5555"},"tags":["inboundcallticket"],"comment":{"value": "This is the description","public":false},"submitter_id":370404079212,"status":"open"}}'
-      expect(a_request(:post, "https://callrail1472494564.zendesk.com/api/v2/tickets").
-      with { |req| req.body == body }).
+      params = {
+        'name' => 'Mr Customer',
+        'email' => 'customer@example.com',
+        'phone' => '555-555-5555',
+        'subject'=> 'CTI Phone Call Ticket',
+        'description' => 'This is a description.'
+      }
+      post zendesk_tickets_create_path(params)
+      request_body = {'ticket':{'subject': 'CTI Phone Call Ticket', 'requester': {'name': 'Mr Customer', 'email': 'customer@example.com', 'phone': '555-555-5555'}, 'tags':['inboundcallticket'], 'comment': {'value': 'This is the description', 'public': false }, 'submitter_id': 370404079212, 'status': 'open'}}
+      expect(a_request(:post, 'https://callrail1472494564.zendesk.com/api/v2/tickets').
+      with { |req| req.body == request_body }).
       to have_been_made
     end
   end
