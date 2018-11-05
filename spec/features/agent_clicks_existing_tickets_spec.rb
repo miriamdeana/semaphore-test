5da341665caa37f3a8231459fac18788afe4f57a
Vicky <vii.zhu@gmail.com>
2018-11-05 13:33:11 -0500

    revert search_results to correct version and update typo on tests

--- spec/features/agent_clicks_existing_tickets_spec.rb
+++ spec/features/agent_clicks_existing_tickets_spec.rb
@@ -6,8 +6,8 @@ require 'rails_helper'
     login_as user
     set_caller_number
     visit("/zendesk/tickets/new?email=support%2Bintegrationtest%40olark.com&id=360248023446&name=Olark&phone=5555555555%2C+4444444444")
-    expect(page).to have_link('existing Zendesk tickets', href: %r[callrail1472494564.zendesk.com/agent/users/#{zendesk_user.id}/tickets])
-    expect(page).not_to have_link('existing Zendesk tickets', href: %r[callrail1472494564.zendesk.com/agent/users/12345/tickets])
-    expect(find_link('existing Zendesk tickets')[:target]).to eq('_blank')
+    expect(page).to have_link('existing Zendesk ticket', href: %r[callrail1472494564.zendesk.com/agent/users/#{zendesk_user.id}/tickets])
+    expect(page).not_to have_link('existing Zendesk ticket', href: %r[callrail1472494564.zendesk.com/agent/users/12345/tickets])
+    expect(find_link('existing Zendesk ticket')[:target]).to eq('_blank')
   end
 end
