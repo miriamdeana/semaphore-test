95f9380c0deb13e464a35563579b30ba201830d2
Vicky <vii.zhu@gmail.com>
2018-11-09 10:14:01 -0500

    connect worker to new ticket form

--- app/workers/zendesk_update_user_worker.rb
+++ app/workers/zendesk_update_user_worker.rb
@@ -2,7 +2,6 @@ require 'callrail_api'
 
 class ZendeskUpdateUserWorker
   include Sidekiq::Worker
-  sidekiq_options retry: false
 
   def perform(zendesk_id, formatted_caller_number)
     user = Zendesk.client.users.find(:id => zendesk_id)
