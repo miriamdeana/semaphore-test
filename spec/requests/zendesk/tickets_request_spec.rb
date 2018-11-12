288da9243be76b3d020ef2e6258cea71f6f339e6
melissakerns <mproud99@gmail.com>
2018-11-12 10:51:27 -0500

    fixed ticket request spec

--- spec/requests/zendesk/tickets_request_spec.rb
+++ spec/requests/zendesk/tickets_request_spec.rb
@@ -1,25 +1,26 @@
 require 'rails_helper'
 
 describe 'Tickets' do
-  before(:each) do
-    stub_request(:post, "https://callrail1472494564.zendesk.com/api/v2/tickets").
-    to_return(status: 200, body: "", headers: {})
-  end
-
   describe 'Ticket Creation' do
     it 'should initiate a post request to Zendesk' do
-      params = {
+      form_params = {
         'name' => 'Mr Customer',
         'email' => 'customer@example.com',
         'phone' => '555-555-5555',
         'subject'=> 'CTI Phone Call Ticket',
         'description' => 'This is a description.'
       }
-      post zendesk_tickets_create_path(params)
-      request_body = {'ticket':{'subject': 'CTI Phone Call Ticket', 'requester': {'name': 'Mr Customer', 'email': 'customer@example.com', 'phone': '555-555-5555'}, 'tags':['inboundcallticket'], 'comment': {'value': 'This is the description', 'public': false }, 'submitter_id': 370404079212, 'status': 'open'}}
-      expect(a_request(:post, 'https://callrail1472494564.zendesk.com/api/v2/tickets').
-      with { |req| req.body == request_body }).
-      to have_been_made
+
+      zendesk_params = {
+        :subject => "CTI Phone Call Ticket",
+        :requester => { :name => form_params['name'], :email => form_params['email'],
+        :phone => form_params['phone'] }, :tags=> ["inboundcallticket"],
+        :comment => { :value => form_params['description'], :public => false },
+        :submitter_id => 370404079212, :status => "open"
+      }
+
+      expect(Zendesk).to receive_message_chain(:client, :tickets, :create).with(zendesk_params)
+      post zendesk_tickets_create_path(form_params)
     end
   end
 end
